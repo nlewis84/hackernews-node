106e928bf067f410b9266bdbbd1f60d974c1c5a6
nllew <n.lewis.perc@gmail.com>
2021-05-10 13:24:12 -0500

    filtering, pagination, & sorting

--- src/index.js
+++ src/index.js
@@ -1,39 +1,65 @@
-const { ApolloServer } = require('apollo-server');
-const { PrismaClient } = require('@prisma/client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
-  
+const { ApolloServer, PubSub } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Subscription = require('./resolvers/Subscription');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
+const fs = require('fs');
+const path = require('path');
+const { getUserId } = require('./utils');
+
+const pubsub = new PubSub();
+
+const prisma = new PrismaClient({
+  errorFormat: 'minimal'
+});
+
 const resolvers = {
-    Query,
-    Mutation,
-    User,
-    Link
-  }
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+};
 
-  const fs = require('fs');
-  const path = require('path');
-  const prisma = new PrismaClient()
-  const { getUserId } = require('./utils');
-
-  const server = new ApolloServer({
-    typeDefs: fs.readFileSync(
-      path.join(__dirname, 'schema.graphql'),
-      'utf8'
-    ),
-    resolvers,
-    context: ({ req }) => {
-      return {
-        ...req,
-        prisma,
-        userId:
-          req && req.headers.authorization
-            ? getUserId(req)
-            : null
-      };
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  },
+  subscriptions: {
+    onConnect: (connectionParams) => {
+      if (connectionParams.authToken) {
+        return {
+          prisma,
+          userId: getUserId(
+            null,
+            connectionParams.authToken
+          )
+        };
+      } else {
+        return {
+          prisma
+        };
+      }
     }
-  });
+  }
+});
 
 server
   .listen()
